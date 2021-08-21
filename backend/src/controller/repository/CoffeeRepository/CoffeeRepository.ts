2454832c975ad93e871e8afba95470caf6665269
wesleyarizio <wesleyarizio26@gmail.com>
2021-08-21 20:01:43 -0300

    changed db type and added method to get coffee by id

--- backend/src/controller/repository/CoffeeRepository/CoffeeRepository.ts
+++ backend/src/controller/repository/CoffeeRepository/CoffeeRepository.ts
@@ -3,23 +3,29 @@ import { Coffee } from "../../../Entity/coffee";
 import { SaveCoffeeControllerArgs } from "../../SaveCoffee/service/SaveCoffeeService";
 
 export class CoffeeRepository {
-  private db: Knex.QueryBuilder<Coffee>;
+  private db: Knex;
 
   constructor(db: Knex) {
-    this.db = db<Coffee>("coffee");
+    this.db = db;
   }
 
   async save({ name, description }: SaveCoffeeControllerArgs): Promise<Coffee> {
-    const [response] = await this.db
+    const [response] = await this.db("coffee")
       .insert({ name, description })
       .returning("*");
 
     return response;
   }
 
-  async getAll(): Promise<Coffee> {
-    const coffees = await this.db.select("*");
+  async getAll(): Promise<Coffee[]> {
+    const coffees = await this.db("coffee").select("*");
 
     return coffees;
+  }
+
+  async getCoffee({ id }: { id: string }): Promise<Coffee> {
+    const [coffee] = await this.db("coffee").select("*").where("id", id);
+
+    return coffee;
   }
 }
