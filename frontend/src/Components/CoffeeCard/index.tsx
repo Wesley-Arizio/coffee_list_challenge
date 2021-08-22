eaf353813e1a21ba3751bd31a662fa0bddf0d25f
wesleyarizio <wesleyarizio26@gmail.com>
2021-08-22 09:52:45 -0300

    added modal to show coffee details

--- frontend/src/Components/CoffeeCard/index.tsx
+++ frontend/src/Components/CoffeeCard/index.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { CoffeeModal } from "../CoffeeModal";
 
 import * as S from "./styles";
 
 export const CoffeeCard: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModal = (): void => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <S.Container>
-      <S.CoffeeImagePreview src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrqSeLYrRmuDySyBVIgsv8ejAfTt0b4BqF3w&usqp=CAU" />
-      <S.ContainerTitle>
-        <S.Title>Coffee</S.Title>
-      </S.ContainerTitle>
-    </S.Container>
+    <>
+      <S.Container onClick={() => handleModal()}>
+        <S.CoffeeImagePreview src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrqSeLYrRmuDySyBVIgsv8ejAfTt0b4BqF3w&usqp=CAU" />
+        <S.ContainerTitle>
+          <S.Title>Coffee</S.Title>
+        </S.ContainerTitle>
+      </S.Container>
+
+      {isModalOpen && <CoffeeModal onClose={handleModal} />}
+    </>
   );
 };
---
+++ frontend/src/Components/CoffeeModal/styles.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  position: fixed;
+
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  background-color: rgba(0, 0, 0, 0.7);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Board = styled.main`
+  width: 80%;
+  max-width: 1500px;
+  min-height: 200px;
+
+  background-color: #fff;
+
+  border-radius: 30px;
+
+  padding: 30px;
+`;
+
+export const BoadHeader = styled.header`
+  flex: 1;
+
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+
+  span {
+    cursor: pointer;
+  }
+`;
+
+export const Title = styled.h1`
+  font-family: Mulish;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 30px;
+  letter-spacing: 0.3px;
+  color: #000;
+  transition: 0.4s;
+`;
+
+export const BoardDescription = styled.main`
+  flex: 3;
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+
+  text-align: justify;
+
+  margin-top: 20px;
+
+  font-family: Mulish;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  letter-spacing: 0.3px;
+  color: #000;
+  transition: 0.4s;
+`;
